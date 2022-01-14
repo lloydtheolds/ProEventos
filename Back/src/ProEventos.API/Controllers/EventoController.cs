f88dac7883163b698c31be492907aa7553b36334
Matheus Felipe Costa Santos <matheus.santos@prodest.es.gov.br>
2022-01-14 13:21:20 -0300

    implementação do contexto

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,58 +13,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        private readonly DataContext _context;
 
-        public IEnumerable<Evento> _evento = new Evento[]{
-                
-                new Evento(){
-                EventoId = 1,
-                Local = "Cariacica",
-                DataEvento = "13/01/2022",
-                Tema = "eSocial",
-                QtdPessoas = 8,
-                Lote = "Pix",
-                ImagemURL = "foto.png"
-
-                }, 
-
-                new Evento(){
-                EventoId = 2,
-                Local = "Vila Velha",
-                DataEvento = "02/02/2019",
-                Tema = "UVV",
-                QtdPessoas = 1,
-                Lote = "CC",
-                ImagemURL = "foto.png"
-
-                },
-
-                new Evento(){
-                EventoId = 3,
-                Local = "Serra",
-                DataEvento = "19/01/2020",
-                Tema = "Passado",
-                QtdPessoas = 2,
-                Lote = "Shopping Vitoria",
-                ImagemURL = "foto.png"
-
-                }
-            };
-    
-        public EventoController()
+        public EventoController(DataContext context)
         {
-
+            _context = context;
         }
 
         [HttpGet]
-        public IEnumerable<Evento> Get( )
+        public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
         public IEnumerable<Evento> GetbyId(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.Where(evento => evento.EventoId == id);
         }
 
         [HttpPost]
--- Back/src/ProEventos.API/Data/DataContext.cs
+++ Back/src/ProEventos.API/Data/DataContext.cs
@@ -7,7 +7,7 @@ namespace ProEventos.API.Data
     {
 
         public DataContext(DbContextOptions<DataContext> options): base(options){}
-
+ 
         public DbSet<Evento> Eventos {get; set; }
     }
-}
+} 
