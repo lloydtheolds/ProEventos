74f375c8cf1979fefe980269402294b1f0669cca
Matheus Felipe Costa Santos <matheus.santos@prodest.es.gov.br>
2022-01-24 14:59:58 -0300

    filtrar eventos, adição de botões e funcionalidades de menu

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,18 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any= [];
+  public eventosfiltrados: any= [];
+
+widthImag: number = 50;
+marginImg: number  = 2;
+exibirImg: boolean = true;
+private _filtroLista: string = '';
+
+public get filtroLista(){
+    return this._filtroLista;
+}
+
+public set filtroLista(value: string){
+  this._filtroLista = value;
+  this.eventosfiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+}
+
+filtrarEventos(filtrarPor: string): any{
+  filtrarPor = filtrarPor.toLocaleLowerCase();
+  return this.eventos.filter(
+    (    evento: { tema: string; local: string;}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+                                                     evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
 
 
-  constructor(private http: HttpClient) { }
+  )
+}
+
+constructor(private http: HttpClient){}
+alterarImg(){
+  this.exibirImg = !this.exibirImg;
+}
+
 
   ngOnInit(): void {
     this.getEventos();
+
   }
 
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosfiltrados = this.eventos;
+      },
       error => console.log(error)
       //complete
     );
